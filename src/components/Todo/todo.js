b9d5d365e7d20e5ec715d570df8bbac7125f7a90
zverexe <tarasent@ukr.net>
2018-02-15 15:48:57 +0200

    added default image

--- src/components/Todo/todo.js
+++ src/components/Todo/todo.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Card, CardImg, CardBody, Button } from 'reactstrap';
 import styles from './todo.scss';
+import defaultImage from '../../assets/default.jpg';
 
 const todo = (props) => {
+  let icon = props.todo.image ? props.todo.image : defaultImage;
     return (
       <div className={styles.card_wrapper}>
         <Card className={styles.card_item}>
-          <CardImg top width="100%" src={props.todo.image} alt="Card image cap" />
+          <CardImg top width="100%" src={icon} alt="Card image cap" />
           <CardBody>
             <h4>{props.todo.name}</h4>
             <p>{props.todo.body}</p>
